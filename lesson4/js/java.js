75037f25e1815c035d9b54740004af06acf00d66
Hicks-Brennan <hic11001@byui.edu>
2019-10-17 20:31:30 -0600

    updated java.js

--- lesson4/js/java.js
+++ lesson4/js/java.js
@@ -3,5 +3,5 @@ document.getElementById('currentYear').innerHTML = new Date().getFullYear();
 document.getElementById('lastUpdated').innerHTML = "Last Updated: " + document.lastModified;
 
 function toggleMenu() {
-    document.getElementByClass("navigation").classList.toggle("responsive");
+    document.getElementByClass("navList")[0].classList.toggle("responsive");
 }
