262df5b3704b0a622bc59589ce18877b46fe8b6f
Hicks-Brennan <hic11001@byui.edu>
2019-10-17 20:24:49 -0600

    add getelementbyclass

--- lesson4/js/java.js
+++ lesson4/js/java.js
@@ -3,5 +3,5 @@ document.getElementById('currentYear').innerHTML = new Date().getFullYear();
 document.getElementById('lastUpdated').innerHTML = "Last Updated: " + document.lastModified;
 
 function toggleMenu() {
-    document.getElementById("navigation").classList.toggle("responsive");
+    document.getElementByClass("navigation").classList.toggle("responsive");
 }
