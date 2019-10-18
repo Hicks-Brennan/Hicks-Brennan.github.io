5c9c4b854b85dba5debd1ce1b0b6cffb0ba1a100
Hicks-Brennan <hic11001@byui.edu>
2019-10-17 20:37:00 -0600

    add hide class

--- lesson4/js/java.js
+++ lesson4/js/java.js
@@ -3,5 +3,5 @@ document.getElementById('currentYear').innerHTML = new Date().getFullYear();
 document.getElementById('lastUpdated').innerHTML = "Last Updated: " + document.lastModified;
 
 function toggleMenu() {
-    document.getElementById("navList")[0].classList.toggle("responsive");
+    document.getElementById("navList")[0].classList.toggle("hide");
 }
