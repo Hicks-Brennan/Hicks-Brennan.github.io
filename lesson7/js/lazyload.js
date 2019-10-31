cf580faa5757f56db83ac43955d300bcdfdffcb7
William Hicks <hic11001@byui.edu>
2019-10-31 17:34:40 -0600

    f

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,26 +1,25 @@
 let imagesToLoad = document.querySelectorAll('img[data-src]');
-
-const imageLoad = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onLoad = () => {
-        image.removeAttribute('data-src');
-    };
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
 };
 
 if('IntersectionObserver' in window) {
-    const imageObserver = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if(item.isIntersecting){
-                imageLoad(item.target);
-                observer.unobserve(item.target);
-            }
-        });
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
     });
     imagesToLoad.forEach((img) => {
-        imageObserver.observe(img);
+      observer.observe(img);
     });
-}
-else {imagesToLoad.forEach((img) => {
-    imageLoad(img)
-});
-}
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
