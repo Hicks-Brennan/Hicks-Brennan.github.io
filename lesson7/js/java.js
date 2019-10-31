fc00ba26163c487dbe17a258a4bf648feff829b3
William Hicks <hic11001@byui.edu>
2019-10-31 14:47:54 -0600

    removed unnecessary things for lesson 7

--- lesson7/js/java.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('currentYear').innerHTML = new Date().getFullYear();
-
-document.getElementById('lastUpdated').innerHTML = "Last Updated: " + document.lastModified;
-
-function toggleMenu() {
-    document.getElementsByClassName("navList")[0].classList.toggle("responsive");
-}
