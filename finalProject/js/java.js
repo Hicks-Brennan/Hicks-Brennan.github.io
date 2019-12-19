e4bf04905433783ed2fc98531e6700818364ffc3
William Hicks <hic11001@byui.edu>
2019-12-19 16:08:43 -0700

    took out current date

--- finalProject/js/java.js
+++ finalProject/js/java.js
@@ -1,7 +1,3 @@
-document.getElementById('currentYear').innerHTML = new Date().getFullYear();
-
-document.getElementById('lastUpdated').innerHTML = "Last Updated: " + document.lastModified;
-
 function toggleMenu() {
     document.getElementsByClassName("navList")[0].classList.toggle("responsive");
 }
